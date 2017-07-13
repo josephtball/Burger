9a3b06862cd982def103499a333cca35093e597e
Joseph Ball <jball321@live.com>
2017-07-13 19:34:45 -0400

    fixed typo

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 
 var connection;
 
-if (proccess.env.JAWSDB_URL) {
-	connection = mysql.createConnection(proccess.env.JAWSDB_URL);
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 	connection = mysql.createConnection({
 		host: 'localhost',
