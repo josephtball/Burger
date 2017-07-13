dc829600a0fa1292726b4db98d97b94606a77a82
Joseph Ball <jball321@live.com>
2017-07-13 19:59:13 -0400

    Testing without remote database.

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,16 @@ var mysql = require('mysql');
 
 var connection;
 
-if (process.env.JAWSDB_URL) {
+/*if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+} else {*/
 	connection = mysql.createConnection({
 		host: 'localhost',
 		user: 'root',
 		password: 'Keyport1',
 		database: 'burgers_db'
 	});
-}
+//}
 
 connection.connect(function(err) {
 	if (err) {
